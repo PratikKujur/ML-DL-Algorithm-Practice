4925bedb8d75b29f51a9f1522418b254e3a03de8
PratikKujur <pratikkujur121@gmail.com>
2024-07-28 19:43:24 +0530

    RandomForest Added

--- LogisticRegression/LogisticRegression.py
+++ LogisticRegression/LogisticRegression.py
@@ -1,33 +1,32 @@
 import numpy as np
 
 def sigmoid(x):
-    return 1/(1+np.exp(-x))
+    return 1 / (1 + np.exp(-x))
 
 class LogisticRegression:
-    def __init__(self,epochs=1000,alpha=0.001):
-        self.epochs=epochs
-        self.alpha=alpha
-        self.weights=None
-        self.bias=None
+    def __init__(self, epochs=1000, alpha=0.001):
+        self.epochs = epochs
+        self.alpha = alpha
+        self.weights = None
+        self.bias = None
 
-    def fit(self,X,y):
-        n_sample,n_feature=X.shape
-        self.weights=np.zeros(n_feature)
-        self.bias=0
+    def fit(self, X, y):
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
+        self.bias = 0
 
         for _ in range(self.epochs):
-            linear=np.dot(X,self.weights)+self.bias
-            y_pred=sigmoid(linear)
+            linear = np.dot(X, self.weights) + self.bias
+            y_pred = sigmoid(linear)
 
-            dw=(1/n_sample)*np.dot(X.T,(y_pred-y))
-            db=(1/n_sample)*np.sum((y_pred-y))
+            dw = (1 / n_samples) * np.dot(X.T, (y_pred - y))
+            db = (1 / n_samples) * np.sum((y_pred - y))
 
-            self.weights=self.weights-self.alpha*dw
-            self.bias=self.bias-self.alpha*db
-    
-    def predict(self,X):
-        linear=np.dot(X,self.weights)+self.bias
-        y_pred=sigmoid(linear)
-        class_pred=[0 if y<=0.5 else 1 for y in y_pred]
+            self.weights -= self.alpha * dw
+            self.bias -= self.alpha * db
+
+    def predict(self, X):
+        linear = np.dot(X, self.weights) + self.bias
+        y_pred = sigmoid(linear)
+        class_pred = [0 if y <= 0.5 else 1 for y in y_pred]
         return class_pred
-
